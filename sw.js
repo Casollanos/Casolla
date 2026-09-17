c50954cb1ba7a063c68d6fc298f5d5e3d0e414b8
Casollanos <ejimenezguerrero48@gmail.com>
2026-09-17 20:22:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,1 @@
-const CACHE='casolla-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/logo-casolla.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
-  }).catch(()=>caches.match(e.request)));
-});
+const VERSION='casolla-v2';const ASSETS=['./','./index.html','./casolla-logo.png','./manifest.webmanifest'];self.addEventListener('install',event=>event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==VERSION).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(VERSION).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))})
